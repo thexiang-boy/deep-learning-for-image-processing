440dc3536ab4c776893b3c7fb9826d9c5578969a
v_xiangyuan <v_xiangyuan@v-xiangyuandeMacBook-Pro.local>
2024-04-07 15:45:48 +0800

    update

--- pytorch_classification/Test1_official_demo/train.py
+++ pytorch_classification/Test1_official_demo/train.py
@@ -14,7 +14,7 @@ def main():
     # 50000张训练图片
     # 第一次使用时要将download设置为True才会自动去下载数据集
     train_set = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                             download=False, transform=transform)
+                                             download=True, transform=transform)
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=36,
                                                shuffle=True, num_workers=0)
 
